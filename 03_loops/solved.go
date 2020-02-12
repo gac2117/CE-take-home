d68d491613947cc5d29f89638052118b69a78bf1
Grace Choi <oiograceoio@gmail.com>
2020-02-12 17:08:28 -0500

    modified loops lesson materials

--- 03_loops/solved.go
+++ 03_loops/solved.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	// Create a for loop that will print out the sum of the numbers between 15 and 25.
-	// Hint: declare a variable called 'sum' outside of the for loop to keep track of the sum
+	// Hint: declare a variable called 'sum' outside of the for loop to keep track of the sum.
 
 	sum := 0
 
@@ -14,7 +14,7 @@ func main() {
 
 	fmt.Println(sum)
 
-	// Create a for loop that will print out numbers 1 to 30
+	// Create a for loop that will print out numbers 1 to 30.
 	// If the number is divisible by 4, print out the word "FizzBuzz" instead of the number.
 	// Hint: Use an if/else statement inside of the for loop.
 
